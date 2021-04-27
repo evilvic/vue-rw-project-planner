a353a1804d525890aaaa5ccd6eb17b4de1c575d8
Víctor Peña Romero <vic.pero@icloud.com>
2021-04-27 08:06:02 -0500

    add create project and basic navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'Home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add new project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import AddProject from '../views/AddProject'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/add',
+    name: 'AddProject',
+    component: AddProject
   }
 ]
 
