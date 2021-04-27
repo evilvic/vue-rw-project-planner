3b15090da4f4e93b0eca1e2277d55478373a63e0
Víctor Peña Romero <vic.pero@icloud.com>
2021-04-26 21:05:40 -0500

    add done and delete methods for projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="!!projects.length">
+      <div 
+        v-for="project in projects" 
+        :key="project.id">
+        <SingleProject 
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
 export default {
   name: 'Home',
-  components: {}
+  components: { SingleProject },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter(project => project.id !== id)
+    },
+    handleComplete(id) {
+      let project = this.projects.find(project => project.id === id)
+      project.complete = !project.complete
+    }
+  }
 }
 </script>
